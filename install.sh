7b755fb85617aa822f9a03e60b6b6bc4dbe1ab8b
BaiLai7513 <bailai7513@users.noreply.github.com>
2026-07-17 09:21:05 +0800

    v260718: restructure repo as flashable module (system/etc/hosts)

---
+++ install.sh
@@ -0,0 +1,22 @@
+SKIPMOUNT=false
+LATESTARTSERVICE=true
+POSTFSDATA=false
+PROPFILE=false
+
+print_modname() {
+  ui_print "*******************************"
+  ui_print "     AF_ADS_FUCK v260718       "
+  ui_print "  anti-fraud + adblock + lock  "
+  ui_print "*******************************"
+}
+
+on_install() {
+  ui_print "- Extracting module files..."
+  unzip -o "$ZIPFILE" -d $MODPATH >&2
+  ui_print "- Done"
+}
+
+set_permissions() {
+  set_perm_recursive $MODPATH 0 0 0755 0644
+  set_perm_recursive $MODPATH/mod 0 0 0755 0755
+}
